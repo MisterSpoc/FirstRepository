818a48c179cd380b56ca7491e6033625c09378d8
CJ <44905837+MisterSpoc@users.noreply.github.com>
2018-11-09 13:09:30 -0600

    Update Euler 004.py

---
+++ Euler 004.py
@@ -0,0 +1,31 @@
+import math
+
+def main():
+
+	compare = 0
+
+	for i in range (100, 999):
+		for j in range (100, 999):
+			product = i*j
+			str_product = str(product)
+			length = len(str_product)
+			if pd(length, str_product, compare) is True:
+				if product > compare:
+					compare = product
+	print ("The largest palindrome product of 3 digit numbers is: ", compare)
+	return
+
+
+def pd(length, str_product, compare):
+
+	test_value = True
+
+	for c in range (1, int(length/2)+1):
+		if str_product[c-1] != str_product[length-c]:
+			test_value = False
+
+	return test_value
+
+
+main()
+input("Press enter to exit")
